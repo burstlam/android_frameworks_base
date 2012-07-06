3c4cdf49e46b1e4233df887aec0e2bf4f535fd45
weiweix.ji <weiweix.ji@intel.com>
2012-07-06 15:34:19 +0800

    Press button on Wired Stereo Headset while httplive audio playback in black screen, music playback automatically.
    The key event is handled by media controller,
    If media controller is hidden, the event can't be handled.
    Try to make vedio view handle the event when media controller hidden.
    
    We only handle media control key events when in fullscreen,
    otherwise the Back or Menu key will be handled by MediaController.
    
    Change-Id: Iccfaf6f47a135d0ace32ec253c400fd82ed877d1
    Author: Weiwei Ji <weiweix.ji@intel.com>
    Signed-off-by: Weiwei Ji <weiweix.ji@intel.com>
    Signed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 29426, 46540

--- core/java/android/webkit/HTML5VideoFullScreen.java
+++ core/java/android/webkit/HTML5VideoFullScreen.java
@@ -28,14 +28,14 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.MediaController;
 import android.widget.MediaController.MediaPlayerControl;
-
+import android.view.KeyEvent;
 
 /**
  * @hide This is only used by the browser
  */
 public class HTML5VideoFullScreen extends HTML5VideoView
     implements MediaPlayerControl, MediaPlayer.OnPreparedListener,
-    View.OnTouchListener {
+    View.OnTouchListener, View.OnKeyListener {
 
     // Add this sub-class to handle the resizing when rotating screen.
     private class VideoSurfaceView extends SurfaceView {
@@ -197,6 +197,7 @@ public class HTML5VideoFullScreen extends HTML5VideoView
         super.onPrepared(mp);
 
         mVideoSurfaceView.setOnTouchListener(this);
+        mVideoSurfaceView.setOnKeyListener(this);
         // Get the capabilities of the player for this stream
         Metadata data = mp.getMetadata(MediaPlayer.METADATA_ALL,
                 MediaPlayer.BYPASS_METADATA_FILTER);
@@ -361,6 +362,22 @@ public class HTML5VideoFullScreen extends HTML5VideoView
         if (mFullScreenMode >= FULLSCREEN_SURFACECREATED
                 && mMediaController != null) {
             toggleMediaControlsVisiblity();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        // Media control key should be dispatched to MediaController to handle.
+        if (mFullScreenMode >= FULLSCREEN_SURFACECREATED
+                && mMediaController != null
+                && (keyCode == KeyEvent.KEYCODE_HEADSETHOOK
+                || keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE
+                || keyCode == KeyEvent.KEYCODE_SPACE
+                || keyCode == KeyEvent.KEYCODE_MEDIA_PLAY
+                || keyCode == KeyEvent.KEYCODE_MEDIA_STOP
+                || keyCode == KeyEvent.KEYCODE_MEDIA_PAUSE)) {
+            return mMediaController.dispatchKeyEvent(event);
         }
         return false;
     }
