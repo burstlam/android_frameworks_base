0f4f24626d2557d787007a5c86a3ef96ce73efc6
krarvind <krarvind@gmail.com>
2013-01-01 17:38:16 -0400

    Framework:Notification Drawer Brightness Slider - (1/2)
    Patchset 10:
    
    fixed the compile errors
    
    Patchset 11:
    - cleanup tabs and whitespaces
    - cleanup unused code
    - cleanup code which doesn't work for 4.2.1
    - change getInt and putInt to getIntForUser and putIntForUser
    - change setBrightness to new aosp handling
    - rearrangments
    - optical changes to fit better to 4.2.1
    - animation same as pw because a pw feature
    
    Patchset 12
    - added forgoten userhandling observer
    
    Patchset 13
    - added license text xml
    
    Patchset 14
    - damn old code - another cleanup unused imports
    
    cheers
    
    Lars
    
    Change-Id: I28c0a61c684be8429331a1445e261c5150a10138
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	packages/SystemUI/res/values/strings.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2564,6 +2564,16 @@ public final class Settings {
         public static final String FCHARGE_ENABLED = "fcharge_enabled";
 
         /**
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_SLIDER = "statusbar_brightness_slider";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_TOGGLES_BRIGHTNESS_LOC = "statusbar_toggles_brightness_loc";
+
+        /**
          * Vibrate when expanding notifications
          * @hide
          */
