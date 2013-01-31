8017f40ef54dba5d85c75a20c94a0512ce93847f
drcmda <drcmda@gmail.com>
2013-01-31 17:02:19 +0000

    PIE, gap & trigger size
    Ps2: Control is very important to us. ;) (dax)
    Ps3: Fix missing PIE when unlocking device... :o (dax)
    
    Change-Id: I16828da9734b6c487eefa1a65538023399122569
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -757,6 +757,7 @@ public class TabletStatusBar extends BaseStatusBar implements
     protected void setSearchLightOn(boolean on){
     }
 
+    private int mShowSearchHoldoff = 0;
     private Runnable mShowSearchPanel = new Runnable() {
         public void run() {
             showSearchPanel();
