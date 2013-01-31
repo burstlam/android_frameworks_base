20127a3a0055b7b6da5813a032eaf214de5ba5c2
burstlam <burstlam@gmail.com>
2013-01-31 21:50:29 +0800

    SystemUI: option to toggle dimming of the notification shade (1/2)
    Allow the user to enable/disable dimming of the notification shade
    pulldown. Check isHighEndGfx() to respect the default setting that
    the device currently supports.
    
    Users on low-end device may want to have this feature, while users
    on high-end devices may want to disable it (possible battery life
    savings and what not).
    
    Change-Id: I730c0eea266541468d2d38ef88e136d85e1e4bfa

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3154,6 +3154,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_VIBRATE_ENABLED = "lockscreen.vibrate_enabled";
 
+        /**
+         * Whether to dim the notification shade when pulling down the status bar
+         * @hide
+         */
+        public static final String NOTIFICATION_SHADE_DIM = "notification_shade_dim";
+
        // PIE //
 
         /**
