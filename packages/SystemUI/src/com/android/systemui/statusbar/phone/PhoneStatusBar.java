c6237065edb6af2dbfd7d28b75b47e7020462133
burstlam <burstlam@gmail.com>
2013-01-31 21:40:31 +0800

    Fix Notification Row Alpha Values
    Change-Id: Ia22da1f81b72721bef3b0c36a5e7ccd85cff92b5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -663,8 +663,6 @@ public class PhoneStatusBar extends BaseStatusBar {
             });
         }
 
-        setNotificationRowHelper();
-
         // Quick Settings (where available, some restrictions apply)
         if (mHasSettingsPanel) {
             // first, figure out where quick settings should be inflated
@@ -1290,6 +1288,9 @@ public class PhoneStatusBar extends BaseStatusBar {
             mNotificationIcons.removeView(remove);
         }
 
+
+        //set alpha for notification pile before it is added
+        setNotificationRowHelper();
         for (int i=0; i<toShow.size(); i++) {
             View v = toShow.get(i);
             if (v.getParent() == null) {
