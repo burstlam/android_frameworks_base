191c115f8956086833f6377e974e2b3f921e8bd2
kecinzer <kecinzer@gmail.com>
2013-02-03 20:06:35 +0100

    FW: Status Bar Hide toggle (1/2)
    Adds new statusbar toggle into power menu navbar control.
    Tested on maguro.
    
    Patch-set 4: Reverted selected button logic
    
    Patch-set 5: Reverted of reverted :)
    
    Patch-set 6: Added czech translation and RC part
    
    Patch-set 7: Added quicksettings tile and shortcut intent
    
    Patch-set 8: Fixed build errors
    
    Patch-set 10: Some czech translations
    
    Patch-set 11: New icons, changed variable names, fixed show logic
    
    Patch-set 12: Rebase
    
    Patch-set 13: Removed previusly added czech translation of other words
    
    Patch-set 14: Resolved conflicts
    
    Change-Id: Iafdbac9fb5830762c12ac0d9a6569723f39a64de
    Signed-off-by: kecinzer <kecinzer@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values-cs/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -134,11 +134,12 @@ public class QuickSettings {
     private static final int LTE_TILE = 22;
     private static final int FAV_CONTACT_TILE = 23;
     private static final int SCREEN_TILE = 24;
-   // private static final int BT_TETHER_TILE = 29;
+   // private static final int BT_TETHER_TILE = 30;
     private static final int SOUND_STATE_TILE = 25;
     private static final int NAVBAR_HIDE_TILE = 26;
     private static final int POWER_MENU_TILE = 27;
     private static final int PROFILE_TILE = 28;
+    private static final int STATUSBAR_TILE = 29;
 
     public static final String USER_TOGGLE = "USER";
     public static final String BRIGHTNESS_TOGGLE = "BRIGHTNESS";
@@ -170,6 +171,7 @@ public class QuickSettings {
     public static final String NAVBAR_HIDE_TOGGLE = "NAVBARHIDE";
     public static final String POWER_MENU_TOGGLE = "POWERMENU";
     public static final String PROFILE_TOGGLE = "PROFILE";
+    public static final String STATUSBAR_TOGGLE = "STATUSBAR";
 
     private static final String DEFAULT_TOGGLES = "default";
 
@@ -254,6 +256,7 @@ public class QuickSettings {
             toggleMap.put(NAVBAR_HIDE_TOGGLE, NAVBAR_HIDE_TILE);
             toggleMap.put(POWER_MENU_TOGGLE, POWER_MENU_TILE);
             toggleMap.put(PROFILE_TOGGLE, PROFILE_TILE);
+            toggleMap.put(STATUSBAR_TOGGLE, STATUSBAR_TILE);
             //toggleMap.put(BT_TETHER_TOGGLE, BT_TETHER_TILE);
         }
         return toggleMap;
@@ -1435,8 +1438,8 @@ public class QuickSettings {
                 mModel.addPowerMenuTile(quick, new QuickSettingsModel.RefreshCallback() {
                     @Override
                     public void refreshView(QuickSettingsTileView view, State state) {
-		       	TextView tv = (TextView) view.findViewById(R.id.powermenu_textview);
-			tv.setText(state.label);
+                        TextView tv = (TextView) view.findViewById(R.id.powermenu_textview);
+                        tv.setText(state.label);
                         tv.setTextSize(1, mTileTextSize);
                         tv.setCompoundDrawablesWithIntrinsicBounds(0, state.iconId, 0, 0);
                     }
@@ -1457,10 +1460,45 @@ public class QuickSettings {
                 mModel.addProfileTile(quick, new QuickSettingsModel.RefreshCallback() {
                     @Override
                     public void refreshView(QuickSettingsTileView view, State state) {
-		       	TextView tv = (TextView) view.findViewById(R.id.profile_textview);
-			tv.setText(state.label);
+                        TextView tv = (TextView) view.findViewById(R.id.profile_textview);
+                        tv.setText(state.label);
                         tv.setTextSize(1, mTileTextSize);
                         tv.setCompoundDrawablesWithIntrinsicBounds(0, state.iconId, 0, 0);
+                    }
+                });
+                break;
+            case STATUSBAR_TILE:
+                quick = (QuickSettingsTileView)
+                        inflater.inflate(R.layout.quick_settings_tile, parent, false);
+                quick.setContent(R.layout.quick_settings_tile_statusbar, inflater);
+                quick.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    boolean StatusbarState = Settings.System.getBoolean(mContext.getContentResolver(),
+                                 Settings.System.STATUSBAR_HIDDEN, false);
+                        Settings.System.putBoolean(mContext.getContentResolver(),
+                                 Settings.System.STATUSBAR_HIDDEN, !StatusbarState);
+                    }
+                });
+                quick.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        Intent intent = new Intent("android.intent.action.MAIN");
+                        // Try to jump directly into UI settings
+                        intent.setComponent(ComponentName.
+                                unflattenFromString("com.aokp.romcontrol/.ROMControlActivity"));
+                        intent.addCategory("android.intent.category.LAUNCHER");
+                        startSettingsActivity(intent);
+                        return true;
+                    }
+                });
+                mModel.addStatusBarTile(quick, new QuickSettingsModel.RefreshCallback() {
+                    @Override
+                    public void refreshView(QuickSettingsTileView view, State state) {
+                        TextView tv = (TextView) view.findViewById(R.id.statusbar_hide_textview);
+                        tv.setCompoundDrawablesWithIntrinsicBounds(0, state.iconId, 0, 0);
+                        tv.setText(state.label);
+                        tv.setTextSize(1, mTileTextSize);
                     }
                 });
                 break;
