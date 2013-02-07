260d997c12f574d06f78e6b3bd0254c49cfafd66
burstlam <burstlam@gmail.com>
2013-02-07 22:44:37 +0800

    Try
    Change-Id: I62811768deaf7c7537275a54f8ccf133d69d2976

--- packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java
@@ -641,7 +641,6 @@ public class NavigationBarView extends LinearLayout {
 
             }
         }
-        getSearchLight().setVisibility(isKeyguardEnabled() ? View.VISIBLE : View.GONE);
         if (mNavBarAutoHide && !isRotating) {
             if (isKeyguardEnabled())
                 mBar.setSearchLightOn(true);
