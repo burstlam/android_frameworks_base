7d20f142feca929f91ba312e0746e50c82d091d1
YuanQY <yuanqingyun@gmail.com>
2013-02-08 10:59:22 +0800

    Keyboard light: Fix the physical keyboard not light when it's visiable.
    Change-Id: I595afd3cb6b422a17ae0f6ec20aa51979db13810
    
    Conflicts:
    	services/java/com/android/server/power/PowerManagerService.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5383,6 +5383,8 @@ updateDisplayMetrics();
     }
 
     private void applyLidSwitchState() {
+        mPowerManager.setKeyboardVisibility(isBuiltInKeyboardVisible());
+
         if (mLidState == LID_CLOSED && mLidControlsSleep) {
             mPowerManager.goToSleep(SystemClock.uptimeMillis());
         }
