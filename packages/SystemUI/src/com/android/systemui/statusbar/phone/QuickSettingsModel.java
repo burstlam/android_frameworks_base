24977064fa3b17bdec205b286aa09f307cd36cce
Bajee11 <bajee11@gmail.com>
2013-02-09 18:05:01 -0500

    Quiet Hours Tile (1/2)
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    
    Change-Id: Iefed35fbb8c138b7e3a80c46ff6a033196bceb9d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -309,6 +309,10 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     private RefreshCallback mPowerMenuCallback;
     private State mPowerMenuState = new State();
 
+    private QuickSettingsTileView mQuietHoursTile;
+    private RefreshCallback mQuietHoursCallback;
+    private State mQuietHoursState = new State();
+
     private QuickSettingsTileView mProfileTile;
     private RefreshCallback mProfileCallback;
     private State mProfileState = new State();
@@ -422,6 +426,8 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
                 refreshNavBarHideTile();
             if (toggle.equals(QuickSettings.POWER_MENU_TOGGLE))
                 refreshPowerMenuTile();
+            if (toggle.equals(QuickSettings.QUIETHOURS_TOGGLE))
+                refreshQuietHoursTile();
             if (toggle.equals(QuickSettings.PROFILE_TOGGLE))
                 refreshPowerMenuTile();
             if (toggle.equals(QuickSettings.STATUSBAR_TOGGLE))
@@ -782,6 +788,35 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
         mPowerMenuState.iconId = R.drawable.ic_qs_powermenu;
         mPowerMenuCallback.refreshView(mPowerMenuTile, mPowerMenuState);
     }
+
+    // Quiet Hours
+    void addQuietHoursTile(QuickSettingsTileView view, RefreshCallback cb) {
+        mQuietHoursTile = view;
+        mQuietHoursCallback = cb;
+        refreshQuietHoursTile();
+    }
+
+    void onQuietHoursChanged() {
+        boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(), Settings.System.QUIET_HOURS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+        mQuietHoursState.enabled = enabled;
+        mQuietHoursState.iconId = enabled
+                ? R.drawable.ic_qs_quiet_hours_on
+                :  R.drawable.ic_qs_quiet_hours_off;
+        mQuietHoursState.label = enabled
+                ? mContext.getString(R.string.quick_settings_quiet_hours_on_label)
+                : mContext.getString(R.string.quick_settings_quiet_hours_off_label);
+
+        if (mQuietHoursTile != null && mQuietHoursCallback != null) {
+            mQuietHoursCallback.refreshView(mQuietHoursTile, mQuietHoursState);
+        }
+    }
+
+    void refreshQuietHoursTile() {
+        if (mQuietHoursTile != null) {
+            onQuietHoursChanged();
+        }
+    }
+
 
     // Profile
     void addProfileTile(QuickSettingsTileView view, RefreshCallback cb) {
