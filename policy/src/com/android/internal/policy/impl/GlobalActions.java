7f9028126b5fa852775545ade25eaa7ea69f994a
burstlam <burstlam@gmail.com>
2013-02-18 21:39:59 +0800

    Show Profile toggle on keyguard
    Change-Id: Id1912e8bfe775462de583d5eb702e9a4529c908c

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -391,7 +391,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                     }
 
                     public boolean showDuringKeyguard() {
-                        return false;
+                        return true;
                     }
 
                     public boolean showBeforeProvisioning() {
