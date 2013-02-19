c7a4374091164733d5699b4b39ac16ac5673abcf
burstlam <burstlam@gmail.com>
2013-02-20 00:12:16 +0800

    Fix notification button
    Change-Id: I9c68cf2af947c287f62eac394e1d0f28d56bb978

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -447,7 +447,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             if (MULTIUSER_DEBUG) Slog.d(TAG, String.format("User setup changed: " +
                     "selfChange=%s userSetup=%s mUserSetup=%s",
                     selfChange, userSetup, mUserSetup));
-            if (mSettingsButton != null && mHasFlipSettings) {
+            if (mSettingsButton != null && !mHasSettingsPanel) {
                 mSettingsButton.setVisibility(userSetup ? View.VISIBLE : View.INVISIBLE);
             }
             if (mSettingsPanel != null) {
@@ -2035,9 +2035,6 @@ public class PhoneStatusBar extends BaseStatusBar {
             return;
         }
 
-        // Settings are not available in setup
-        if (!mUserSetup) return;
-
         if (mHasFlipSettings) {
             mNotificationPanel.expand();
             if (mFlipSettingsView.getVisibility() != View.VISIBLE) {
@@ -2051,8 +2048,6 @@ public class PhoneStatusBar extends BaseStatusBar {
     }
 
     public void switchToSettings() {
-        // Settings are not available in setup
-        if (!mUserSetup) return;
 
         mFlipSettingsView.setScaleX(1f);
         mFlipSettingsView.setVisibility(View.VISIBLE);
@@ -2109,8 +2104,6 @@ public class PhoneStatusBar extends BaseStatusBar {
     }
 
     public void flipToSettings() {
-        // Settings are not available in setup
-        if (!mUserSetup) return;
 
         if (mFlipSettingsViewAnim != null) mFlipSettingsViewAnim.cancel();
         if (mPowerWidgetAnim != null) mPowerWidgetAnim.cancel();
