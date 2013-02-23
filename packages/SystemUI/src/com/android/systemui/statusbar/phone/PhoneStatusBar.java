cbc54979e6fa3c7c1fba42ef245257d14a7d7bb4
Stevespear426 <stephen.k.spear@gmail.com>
2013-02-23 06:02:23 +0100

    AwesomeAction: Make more awesomeer
    Patchset 1:
    - Switched to static methods and enums
    - Moved Navbar helpers to its own class to
      be moved to FW for RC navbar recode.
    - Created broadcast reicever
      use it like this...
    
         Intent i = new Intent();
         i.setAction("com.android.systemui.aokp.LAUNCH_ACTION");
         i.putExtra("action", "**string you want AwesomeAction to launch**");
         mContext.sendBroadcast(i);
    
    Removed screenshot option for 2 reasons
    1) Security
    2) Can't be made static.
    
    Patchset 2/3 : white space / string derp
    
    Patchset 4: mini optimization in fromString function
    
    Patchset 5/6 : clean up. Should be good to go now.
    
    Change-Id: I9159a70082058f4dd1afc47c9d57a6f4b4a6bfb1
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/NavigationBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2928,7 +2928,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             public void run() {
                     doubleClickCounter = 0;
                     animateCollapsePanels();
-                    AwesomeAction.getInstance(mContext).launchAction(mClockActions[shortClick]);
+                    AwesomeAction.launchAction(mContext, mClockActions[shortClick]);
             }
         };
 
@@ -2945,7 +2945,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                     mHandler.removeCallbacks(DelayShortPress);
                     vibrate();
                     animateCollapsePanels();
-                    AwesomeAction.getInstance(mContext).launchAction(mClockActions[doubleClick]);
+                    AwesomeAction.launchAction(mContext, mClockActions[doubleClick]);
                     mHandler.postDelayed(ResetDoubleClickCounter, 50);
                 } else {
                     doubleClickCounter = doubleClickCounter + 1;
@@ -2955,7 +2955,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             } else {
                 vibrate();
                 animateCollapsePanels();
-                AwesomeAction.getInstance(mContext).launchAction(mClockActions[shortClick]);
+                AwesomeAction.launchAction(mContext, mClockActions[shortClick]);
             }
 
         }
@@ -3011,7 +3011,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         @Override
         public boolean onLongClick(View v) {
             animateCollapsePanels();
-            AwesomeAction.getInstance(mContext).launchAction(mClockActions[longClick]);
+            AwesomeAction.launchAction(mContext, mClockActions[longClick]);
             return true;
         }
     };
--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -213,7 +213,6 @@ public class TabletStatusBar extends BaseStatusBar implements
     private int doubleClickCounter = 0;
     public String[] mClockActions = new String[3];
     private boolean mClockDoubleClicked;
-    private AwesomeAction mAwesomeAction;
     private View mDateTimeView;
 
     public Context getContext() { return mContext; }
@@ -546,7 +545,6 @@ public class TabletStatusBar extends BaseStatusBar implements
         mNavBarView.setDisabledFlags(mDisabled);
         mNavBarView.setBar(this);
         mNavBarView.getSearchLight().setOnTouchListener(mHomeSearchActionListener);
-        mAwesomeAction = new AwesomeAction(mContext);
 
         LayoutTransition lt = new LayoutTransition();
         lt.setDuration(250);
@@ -768,7 +766,7 @@ public class TabletStatusBar extends BaseStatusBar implements
         public void run() {
                 doubleClickCounter = 0;
                 animateCollapsePanels();
-                mAwesomeAction.launchAction(mClockActions[shortClick]);
+                AwesomeAction.launchAction(mContext, mClockActions[shortClick]);
         }
     };
 
@@ -785,7 +783,7 @@ public class TabletStatusBar extends BaseStatusBar implements
                     mHandler.removeCallbacks(DelayShortPress);
                     v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                     animateCollapsePanels();
-                    mAwesomeAction.launchAction(mClockActions[doubleClick]);
+                    AwesomeAction.launchAction(mContext, mClockActions[doubleClick]);
                     mHandler.postDelayed(ResetDoubleClickCounter, 50);
                 } else {
                     doubleClickCounter = doubleClickCounter + 1;
@@ -795,7 +793,7 @@ public class TabletStatusBar extends BaseStatusBar implements
             } else {
                 v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
                 animateCollapsePanels();
-                mAwesomeAction.launchAction(mClockActions[shortClick]);
+                AwesomeAction.launchAction(mContext, mClockActions[shortClick]);
             }
         }
     };
@@ -805,7 +803,7 @@ public class TabletStatusBar extends BaseStatusBar implements
         public boolean onLongClick(View v) {
             animateCollapsePanels();
             v.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-            mAwesomeAction.launchAction(mClockActions[longClick]);
+            AwesomeAction.launchAction(mContext, mClockActions[longClick]);
             return true;
         }
     };
