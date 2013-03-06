30c45ee2d84cb72277ed999e7797d4920902c79d
drcmda <drcmda@gmail.com>
2013-03-06 23:00:00 +0000

    PWM: disable HW overlays for PIE
    SurfaceFlinger wont wake up the GPU in SysUI's fullscreen state,
    probably because it does not occupy any tangible region. That sets poor
    PIE into software rendering, which has bad effects on transparency and
    mdp compositioning. Disabling HW overlays as long as PIE is active seems
    like a good alternative for now.
    
    Change-Id: Ie7ed720edc5bbdd0cfb79c34a22996cf15fcd79b
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -53,6 +53,7 @@ import android.os.IRemoteCallback;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.Parcel;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -551,6 +552,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mVolBtnMusicControls;
     private boolean mIsLongPress;
 
+    // HW overlays state
+    int mDisableOverlays = 0;
+
     private int mSystemDpi = 0;
     private int mSystemUiDpi = 0;
     private int mSystemUiLayout = 0;
@@ -1006,6 +1010,42 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         });
     }
 
+    private int updateFlingerOptions() {
+        int disableOverlays = 0;
+        try {
+            IBinder flinger = ServiceManager.getService("SurfaceFlinger");
+            if (flinger != null) {
+                Parcel data = Parcel.obtain();
+                Parcel reply = Parcel.obtain();
+                data.writeInterfaceToken("android.ui.ISurfaceComposer");
+                flinger.transact(1010, data, reply, 0);
+                reply.readInt();
+                reply.readInt();
+                reply.readInt();
+                reply.readInt();
+                disableOverlays = reply.readInt();
+                reply.recycle();
+                data.recycle();
+            }
+        } catch (RemoteException ex) {
+        }
+        return disableOverlays;
+    }
+
+    private void writeDisableOverlaysOption(int state) {
+        try {
+            IBinder flinger = ServiceManager.getService("SurfaceFlinger");
+            if (flinger != null) {
+                Parcel data = Parcel.obtain();
+                data.writeInterfaceToken("android.ui.ISurfaceComposer");
+                data.writeInt(state);
+                flinger.transact(1008, data, null, 0);
+                data.recycle();
+            }
+        } catch (RemoteException ex) {
+        }
+    }
+
     /** {@inheritDoc} */
     public void init(Context context, IWindowManager windowManager,
             WindowManagerFuncs windowManagerFuncs) {
@@ -1023,6 +1063,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mOrientationListener.setCurrentRotation(windowManager.getRotation());
         } catch (RemoteException ex) { }
 
+        mDisableOverlays = updateFlingerOptions();
         updateHybridLayout();
 
         mSettingsObserver = new SettingsObserver(mHandler);
@@ -1052,6 +1093,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     false, new ContentObserver(new Handler()) {
             @Override
             public void onChange(boolean selfChange) {
+
+                boolean expDesktop = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1;
+
+                if (!expDesktop) {
+                    // When leaving fullscreen switch back to original HW state
+                    int disableOverlays = updateFlingerOptions();
+                    if (disableOverlays != mDisableOverlays) writeDisableOverlaysOption(mDisableOverlays);
+                } else {
+                    // Before switching to fullscreen safe current HW state, then disable
+                    mDisableOverlays = updateFlingerOptions();
+                    writeDisableOverlaysOption(1);
+                }
+
+                updateHybridLayout();
+                update(false);
 
                 // Restart default launcher activity
                 final PackageManager mPm = mContext.getPackageManager();
