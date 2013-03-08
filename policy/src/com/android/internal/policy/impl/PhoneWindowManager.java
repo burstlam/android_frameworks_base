ef222fe9ef84f5d72d4dffaa1a688a56d0a7f437
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-08 09:31:40 +0100

    SystemUI: Reboot Toggle
    onClick: 3-way Reboot Menu
    onLongClick: Screen off then reboot
    
    ToDo: Allow invert of onClick<->onLongClick
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Ibc601112ede88490c1dd6e5d01122e5037244150
    
    Conflicts:
    	packages/SystemUI/res/values/strings_toggles.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -372,6 +372,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 			}
             if (action.equals(Intent.ACTION_SCREENSHOT)) {
                 takeScreenshot();
+			}
+			if (action.equals(Intent.ACTION_REBOOTMENU)) {
+				showRebootDialog();
             }
         }
 
@@ -384,6 +387,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 filter.addAction(Intent.ACTION_POWERMENU_REBOOT);
                 filter.addAction(Intent.ACTION_POWERMENU_PROFILE);
 				filter.addAction(Intent.ACTION_SCREENSHOT);
+				filter.addAction(Intent.ACTION_REBOOTMENU);
                 mContext.registerReceiver(mPowerMenuReceiver, filter);
             }
         }
@@ -900,6 +904,19 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 	            mGlobalActions = new GlobalActions(mContext, mWindowManagerFuncs);
 	        }
 	        mGlobalActions.showProfileDialog();
+    }
+
+    public void showRebootDialog() {
+        if (mGlobalActions == null) {
+            mGlobalActions = new GlobalActions(mContext, mWindowManagerFuncs);
+        }
+        final boolean keyguardShowing = keyguardIsShowingTq();
+        mGlobalActions.createRebootDialog().show();
+        if (keyguardShowing) {
+            // since it took two seconds of long press to bring this up,
+            // poke the wake lock so they have some time to see the dialog.
+            mKeyguardMediator.userActivity();
+        }
     }
 
     boolean isDeviceProvisioned() {
