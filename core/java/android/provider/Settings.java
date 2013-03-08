619a11edb9fbd0a6528063d1047c02f18cadcbf7
blk_jack <jcorcoran+github@gmail.com>
2013-03-08 15:46:36 -0500

    InputMethodService: Auto Keyboard Rotation (1/2)
    Force auto-rotate when keyboard is shown.
    Configurable auto-rotate sensor timeout after keyboard is hidden.
    
    Change-Id: I56bc333e91702a9227409e35fce28567c435d16c
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3344,6 +3344,12 @@ public final class Settings {
         */
         public static final String DISABLE_FULLSCREEN_KEYBOARD = "disable_fullscreen_keyboard";
 
+        /**
+         * Automatic keyboard rotation timeout.  0 to disable completely.
+         * @hide
+         */
+        public static final String KEYBOARD_ROTATION_TIMEOUT = "keyboard_rotation_timeout";
+
        // PIE //
 
         /**
