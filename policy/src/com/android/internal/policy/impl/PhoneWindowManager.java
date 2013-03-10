e531d8e21ff30f2e9962e94442d2596e0a3a387d
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-10 05:58:43 +0100

    SystemUI: Screenshot Toggle
    onClick: Screenshot now(collpase status bar)
    onLongClick: Starts 'timer' and screenshot after timer expires
    
    PS2: Remove rebootmenu calls from PWM and add 500ms delay to allow statusbar to
             fully collapse
    
    ToDo:
    * Allow user set time in RC
    * Allow inverting onClick and onLongClick
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: I2cebb23e530a92901319b7171c3934125568c3ab
    
    Conflicts:
    	core/java/android/content/Intent.java
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/res/values/strings_toggles.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -369,8 +369,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             if (action.equals(Intent.ACTION_POWERMENU_REBOOT)) {
                 mWindowManagerFuncs.rebootTile();
             }
-	    if (action.equals(Intent.ACTION_POWERMENU_PROFILE)) {
+		    if (action.equals(Intent.ACTION_POWERMENU_PROFILE)) {
                showGlobalActionsProfileDialog();
+			}
+            if (action.equals(Intent.ACTION_SCREENSHOT)) {
+                takeScreenshot();
             }
         }
 
@@ -382,6 +385,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 filter.addAction(Intent.ACTION_POWERMENU);
                 filter.addAction(Intent.ACTION_POWERMENU_REBOOT);
                 filter.addAction(Intent.ACTION_POWERMENU_PROFILE);
+				filter.addAction(Intent.ACTION_SCREENSHOT);
                 mContext.registerReceiver(mPowerMenuReceiver, filter);
             }
         }
@@ -3645,7 +3649,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     };
 
     // Assume this is called from the Handler thread.
-    private void takeScreenshot() {
+    public void takeScreenshot() {
         synchronized (mScreenshotLock) {
             if (mScreenshotConnection != null) {
                 return;
