85405df8aab3e688a8411a6b0fe6707e12dde286
Zaphod <mwilson@acesappraisal.com>
2013-04-14 03:59:47 -0400

    FW : Screen Sizing for TabUI when HiddenNav Enabled
    Hidden Nav is not available to Tablets, but it could be
    selected in RC for Phablet/Phone.  If UI then switched
    to Tablet, PhoneWindowManager was calculating window sizes
    as if NavHide was on.
    
    Change-Id: Ib700fc944dcfb0ad42e7c651ee415f539ed5d134
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -1399,9 +1399,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 Settings.System.USER_UI_MODE,mStockUIMode)) {
             resetScreenHelper();
         }
-        if (NavHide != mNavBarAutoHide) {
+        if (NavHide != mNavBarAutoHide && mUserUIMode != 1) {
             mNavBarAutoHide = NavHide;
-            resetScreenHelper();
+            resetScreenHelper(); 
         }
     }
 
