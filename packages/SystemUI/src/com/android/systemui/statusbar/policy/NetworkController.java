b741b9f51050fce713e3022e3379e8862151fc51
Rohan Mathur <rohanmathur34@gmail.com>
2013-04-14 09:25:33 -0400

    Remove trailing tab
    Change-Id: I5b715bd014db2b82395c7a3cd91f7381c83667e7

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -1214,7 +1214,7 @@ public class NetworkController extends BroadcastReceiver {
             }
         }
 
-        if (customLabel != null && customLabel.length() > 0) {	
+        if (customLabel != null && customLabel.length() > 0) {
             combinedLabel = customLabel;
             mobileLabel = customLabel;
         }
