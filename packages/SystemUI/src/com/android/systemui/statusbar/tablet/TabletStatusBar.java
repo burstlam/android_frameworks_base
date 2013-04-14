7da730a6a6e97384e5bd5a533d80e8c041645596
Zaphod <mwilson@acesappraisal.com>
2013-04-13 22:08:01 -0400

    FW:  Bring Transparency to Tablet UI {WIP}
    This should bring Transparency settings to TabletUI.
    
    Change-Id: I89b4e97f70af2e9ef5245524c7bd765293ef044b
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -242,7 +242,7 @@ public class TabletStatusBar extends BaseStatusBar implements
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                     | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
                     | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
-                PixelFormat.OPAQUE);
+                PixelFormat.TRANSPARENT);
 
         // We explicitly leave FLAG_HARDWARE_ACCELERATED out of the flags.  The status bar occupies
         // very little screen real-estate and is updated fairly frequently.  By using CPU rendering
@@ -661,6 +661,10 @@ public class TabletStatusBar extends BaseStatusBar implements
         SettingsObserver settingsObserver = new SettingsObserver(new Handler());
         settingsObserver.observe();
         updateSettings();
+        mNavBarView.setTransparencyManager(mTransparencyManager);
+        mTransparencyManager.setStatusbar(mStatusBarView);
+        mTransparencyManager.setNavbar(mNavBarView);
+        mTransparencyManager.update();
         return sb;
     }
 
@@ -1089,6 +1093,7 @@ public class TabletStatusBar extends BaseStatusBar implements
                 mHandler.sendEmptyMessage(MSG_CLOSE_RECENTS_PANEL);
            }
         }
+        mTransparencyManager.update();
     }
 
     private void setNavigationVisibility(int visibility) {
@@ -1229,6 +1234,7 @@ public class TabletStatusBar extends BaseStatusBar implements
     }
 
     public void topAppWindowChanged(boolean showMenu) {
+        mTransparencyManager.update();
         if (mPieControlPanel != null)
             mPieControlPanel.setMenu(showMenu);
 
