42298e038244355afda271940e3db321a4557b89
Lars Greiss <kufikugel@googlemail.com>
2013-04-16 20:08:19 +0200

    Frameworks: Ehance toggleNotificationShade feature
    allows to toggle notification drawer in any situations
    on fullscreen mode as well in expanded desktop mode
    and on all other features we have
    cheers
    
    Change-Id: I56185f2c48700d9f2d69c02585810db4e95e2e32
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4626,6 +4626,14 @@ public final class Settings {
         public static final String HIDE_STATUSBAR = "hide_statusbar";
 
         /**
+         * Whether Status Bar is currently hidden or not for notification
+         * toggle notification shade
+         *
+         * @hide
+         */
+        public static final String TOGGLE_NOTIFICATION_SHADE = "toggle_notification_shade";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
