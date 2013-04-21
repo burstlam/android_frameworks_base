246adaaa203ab9e904d26a9621ce795fb7c1d893
burstlam <burstlam@gmail.com>
2013-04-21 22:11:33 +0800

    Derp
    Change-Id: I67f504e32834643bfba62003ce424c14f6305f4f

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -493,10 +493,6 @@ public class TabletStatusBar extends BaseStatusBar implements
         } catch (RemoteException ex) {
         }
 
-        // Create a dummy view to force the screen to redraw
-        mDummyView = new View(mContext);
-        mWindowManager.addView(mDummyView, getDummyTriggerLayoutParams(mContext,Gravity.TOP));
-
         // Overload screen with views that literally do nothing, thank you Google
         int dummyGravity[] = {Gravity.LEFT, Gravity.TOP, Gravity.RIGHT, Gravity.BOTTOM};  
         for (int i = 0; i < 4; i++ ) {
