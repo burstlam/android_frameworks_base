4e22c94cb65962f46279145e239dae163c30db73
burstlam <burstlam@gmail.com>
2013-04-24 05:05:33 +0800

    PIE : Refactor to Expanded Toggle enable is fine
    Change-Id: I469b22936825136c23d17307687004b62ee98935

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -541,9 +541,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         boolean pie = Settings.System.getInt(mContext.getContentResolver(),
                       Settings.System.PIE_CONTROLS, 0) == 1;
         boolean sbexpanded = Settings.System.getInt(mContext.getContentResolver(),
-                             Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1 || 
-                             Settings.System.getInt(mContext.getContentResolver(),
-                             Settings.System.EXPANDED_DESKTOP_STYLE, 0) == 2;
+                             Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1;
         boolean autoHide =   Settings.System.getBoolean(mContext.getContentResolver(),
                              Settings.System.NAV_HIDE_ENABLE, false) == true;
         boolean navbarZero = Settings.System.getInt(mContext.getContentResolver(),
