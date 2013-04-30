0cf807d61b6e70c6ddec49430bbd892dde4355d4
burstlam <burstlam@gmail.com>
2013-04-30 21:15:59 +0800

    Merge Derp
    Change-Id: I229d25a3dcc099d722a78c0d63dc24147c36183b

--- core/java/com/android/internal/util/aokp/AwesomeConstants.java
+++ core/java/com/android/internal/util/aokp/AwesomeConstants.java
@@ -62,8 +62,6 @@ public class AwesomeConstants {
         ACTION_EVENT         { @Override public String value() { return "**event**";}},
         ACTION_TODAY         { @Override public String value() { return "**today**";}},
         ACTION_ALARM         { @Override public String value() { return "**alarm**";}},
-        ACTION_UNLOCK        { @Override public String value() { return "**unlock**";}},
-        ACTION_CAMERA        { @Override public String value() { return "**camera**";}},
         ACTION_NULL          { @Override public String value() { return "**null**";}},
         ACTION_UNLOCK        { @Override public String value() { return "**unlock**";}},
         ACTION_CAMERA        { @Override public String value() { return "**camera**";}},
