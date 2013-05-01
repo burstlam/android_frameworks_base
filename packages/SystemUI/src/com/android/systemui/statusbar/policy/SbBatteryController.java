608316a3f7217d0402a0fdf36cba9d6037d1eb8d
burstlam <burstlam@gmail.com>
2013-05-01 09:52:28 +0800

    fix leak upon theme changes
    Change-Id: I1a4931710b6c1f5ff115bda4cedfa0f505108d65

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
@@ -125,6 +125,10 @@ public class SbBatteryController extends LinearLayout {
         mChangeCallbacks.add(cb);
     }
 
+    public void removeStateChangedCallback(BatteryStateChangeCallback cb) {
+        mChangeCallbacks.remove(cb);
+    }
+
     private BroadcastReceiver mBatteryBroadcastReceiver = new BroadcastReceiver() {
 
         @Override
