f62203d1fe6c7ef5ebaacd513ce1fe7ebda56b8d
Lars Greiss <kufikugel@googlemail.com>
2013-05-02 22:32:36 +0200

    Fix: reported disable redundant statusbar information
    Current state we introduced with first SlimPIE patch is that
    if the user disable statusbar information (clock/date/airplanemodeicon)
    that it is always hidden...even if the user set SlimPIE to only on
    expanded desktop mode. That is not consistent and some user
    wonder why the do not have this information any more in normal mode.
    
    This commit fixes this.....respects also the expanded mode hide
    only navbar.....as well a small cleanup from old battery code.
    Airplanemodeicon hide I removed from redundant hide information now....
    to change that on the fly would need an observer and a bit code rearrange
    which does not fit the use compared to the resources we would use then.
    Beside that all other icons are as well shown because our PIE does not
    show signal strength etc.
    
    Cheers
    
    Change-Id: I53f1fed10a13551e7642e5b00c1d3b88a9622ab0
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/CircleBattery.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
@@ -284,6 +284,12 @@ public class SbBatteryController extends LinearLayout {
             resolver.registerContentObserver(Settings.System
                     .getUriFor(Settings.System.STATUSBAR_BATTERY_TEXT_COLOR), false,
                     this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.PIE_DISABLE_STATUSBAR_INFO), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.PIE_CONTROLS), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.EXPANDED_DESKTOP_STATE), false, this);
         }
 
         @Override
@@ -297,6 +303,21 @@ public class SbBatteryController extends LinearLayout {
         ContentResolver cr = mContext.getContentResolver();
         mBatteryStyle = Settings.System.getInt(cr,
                 Settings.System.STATUSBAR_BATTERY_ICON, 0);
+
+        boolean disableStatusBarInfo = Settings.System.getInt(cr,
+                Settings.System.PIE_DISABLE_STATUSBAR_INFO, 0) == 1;
+        if (disableStatusBarInfo) {
+            // call only the settings if statusbar info is really hidden
+            int pieMode = Settings.System.getInt(cr,
+                    Settings.System.PIE_CONTROLS, 0);
+            boolean expandedDesktopState = Settings.System.getInt(cr,
+                    Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1;
+
+            if (pieMode == 2
+                || pieMode == 1 && expandedDesktopState) {
+                mBatteryStyle = STYLE_HIDE;
+            }
+        }
 
         switch (mBatteryStyle) {
             case STYLE_ICON_ONLY:
