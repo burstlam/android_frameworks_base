fadd91d034cd45cfc4db79e17f3ed6e2b06b8801
Stevespear426 <stephen.k.spear@gmail.com>
2013-05-03 21:19:00 +0200

    FW: Ribbons - Long swipe App window.
    Window similar to recents with all the users apps.
    Options to control app color, background color, and
    background opacity.
    
    On touch listeners for targets and close button
    Make dividers colorize with text.
    
    Fixed animations make columns spacing work automagiclly
    
    Change-Id: I6147dee1e97436f252a7dae349d1eff1d6e5dbf9

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -40,6 +40,7 @@ import com.android.systemui.statusbar.tablet.StatusBarPanel;
 import com.android.systemui.statusbar.WidgetView;
 import com.android.systemui.TransparencyManager;
 import com.android.systemui.aokp.AokpSwipeRibbon;
+import com.android.systemui.aokp.AppWindow;
 
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -129,6 +130,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     private WidgetView mWidgetView;
     private AokpSwipeRibbon mAokpSwipeRibbon;
+    private AppWindow mAppWindow;
 
     protected static final boolean ENABLE_INTRUDERS = false;
 
@@ -412,6 +414,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         mAokpSwipeRibbon = new AokpSwipeRibbon(mContext,null,"bottom");
         mAokpSwipeRibbon = new AokpSwipeRibbon(mContext,null,"left");
         mAokpSwipeRibbon = new AokpSwipeRibbon(mContext,null,"right");
+        mAppWindow = new AppWindow(mContext,null);
         disable(switches[0]);
         setSystemUiVisibility(switches[1], 0xffffffff);
         topAppWindowChanged(switches[2] != 0);
