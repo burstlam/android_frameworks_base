f9edef8deac70452c67333a47b80d381be2f1a0d
burstlam <burstlam@gmail.com>
2013-05-07 23:56:46 +0800

    Network Traffic : respect custom font size change
    Change-Id: Ifd52ef7b5991608e23e5e3cc555697c6cbf910ff

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java
@@ -33,6 +33,8 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.internal.util.aokp.StatusBarHelpers;
+
 public class Traffic extends TextView {
     private boolean mAttached;
     TrafficStats mTrafficStats;
@@ -43,6 +45,8 @@ public class Traffic extends TextView {
     float totalRxBytes;
 
     protected int mTrafficColor;
+    protected int mStockFontSize;
+    protected int mFontSize;
 
     class SettingsObserver extends ContentObserver {
         SettingsObserver(Handler handler) {
@@ -85,6 +89,7 @@ public class Traffic extends TextView {
         if (!mAttached) {
             mAttached = true;
             mTrafficColor = getTextColors().getDefaultColor();
+            mStockFontSize = StatusBarHelpers.pixelsToSp(mContext,getTextSize());
             IntentFilter filter = new IntentFilter();
             filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
             getContext().registerReceiver(mIntentReceiver, filter, null,
@@ -161,15 +166,20 @@ public class Traffic extends TextView {
 
     private void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
-        int newColor = 0;
 
+        mFontSize = Settings.System.getInt(resolver,
+			Settings.System.STATUSBAR_FONT_SIZE, mStockFontSize);
+
+        int newColor = 0;
         newColor = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_TRAFFIC_COLOR, mTrafficColor);
         if (newColor < 0 && newColor != mTrafficColor) {
             mTrafficColor = newColor;
             setTextColor(mTrafficColor);
         }
-
+        if (mFontSize != getTextSize()){
+			setTextSize(mFontSize);
+        }
         showTraffic = (Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_TRAFFIC, 1) == 1);
         if (showTraffic && getConnectAvailable()) {
