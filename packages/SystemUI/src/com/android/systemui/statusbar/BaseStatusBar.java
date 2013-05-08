224cee27665d0f972400fa875ba835f95e6f138f
Stevespear426 <stephen.k.spear@gmail.com>
2013-05-08 02:27:33 +0200

    FW: Ribbon - Toggles
    Fix for some toggles not getting set properly
    fix user toggle fc
    fix some toggles hieght being HUGE
    
    respect users click vib setting.
    
    Change-Id: Ia225e822485b715e6daddeb7b62e232e72db5e36
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -101,7 +101,6 @@ import com.android.systemui.statusbar.view.PieStatusPanel;
 import com.android.systemui.statusbar.view.PieExpandPanel;
 import com.android.systemui.statusbar.WidgetView;
 import com.android.systemui.TransparencyManager;
-import com.android.systemui.aokp.AokpSwipeRibbon;
 import com.android.systemui.aokp.AppWindow;
 
 import java.util.ArrayList;
@@ -127,7 +126,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected int mCurrentUIMode;
 
     private WidgetView mWidgetView;
-    private AokpSwipeRibbon mAokpSwipeRibbon;
     private AppWindow mAppWindow;
 
     protected static final boolean ENABLE_INTRUDERS = false;
@@ -408,9 +406,6 @@ public abstract class BaseStatusBar extends SystemUI implements
         createAndAddWindows();
         // create WidgetView
         mWidgetView = new WidgetView(mContext,null);
-        mAokpSwipeRibbon = new AokpSwipeRibbon(mContext,null,"bottom");
-        mAokpSwipeRibbon = new AokpSwipeRibbon(mContext,null,"left");
-        mAokpSwipeRibbon = new AokpSwipeRibbon(mContext,null,"right");
         mAppWindow = new AppWindow(mContext,null);
         disable(switches[0]);
         setSystemUiVisibility(switches[1], 0xffffffff);
