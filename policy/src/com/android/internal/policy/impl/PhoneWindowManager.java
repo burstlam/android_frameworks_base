a3bb0429881e57803960f1cf22d9e12235ac7cac
Stevespear426 <stephen.k.spear@gmail.com>
2013-05-09 17:37:09 +0200

    FW: Ribbon v1.0 overhaul
    Thickness Increase max && decrease min
    Configurable long swipe and long press swipe areas
    
    consolidate and cache views for smoother animations
    User adjustable animantion duration
    
    Add dismiss options for swipe ribbon
     - Always dismiss on click
     - never dismiss on click
     - Don't dismiss for navbar action
    
    fix app window crash if user has fast fingers and to much time
      on their hands...
    
    Swipe ribbon drag handles now get their own location, height,
       width, and opacity.
    
    Hide apps from app window
    Hooks to dismiss app window if softkeys or navbar buttons pressed
    
    Fix app window to pull all components also IE camera and messenger.
    
    Rub some unicorn polish on the on touch glow. The giant boxes were
    a bit ridiculous.
    
    If ribbon is empty but toogles are not, just show toggles no flipper.
    
    add adb wireless to toggle map
    
    Phase 1 of the Animation chooser. Simple helpers to let users
    choose the animation for the swipe ribbon and app window.
    ill be adding some fancier animations for now just some of
    the stock AOSP one.
    
    Add lots o animation options. Also preload animations instead
    of creating them at run time, should smooth things out a bit.
    
    Keyboard IME hide, Vertical spacing App window, Cache resources.
    
    Fix IME hide when navbar hidden.
    
    Longpress on app window now takes you to app info.
    
    Change-Id: I7c59fc915473fe320639eda446a735dee8500efa
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/strings.xml
    	packages/SystemUI/res/values/dimens.xml

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -329,6 +329,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mUserNavBarWidth;
     int mUserUIMode; //User selected UI Mode (Phablet/Tablet, etc)
     int mStockUIMode; // UI Mode as dictated by screen size.
+    private Intent closeAppWindow;
 
     WindowState mKeyguard = null;
     KeyguardViewMediator mKeyguardMediator;
@@ -1673,6 +1674,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     public void updateSettings() {
+        closeAppWindow = new Intent();
+        closeAppWindow.setAction("com.android.systemui.ACTION_HIDE_APP_WINDOW");
         ContentResolver resolver = mContext.getContentResolver();
 
         mExpandedStyle = Settings.System.getInt(mContext.getContentResolver(),
@@ -2658,6 +2661,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     /** {@inheritDoc} */
     @Override
     public long interceptKeyBeforeDispatching(WindowState win, KeyEvent event, int policyFlags) {
+        mContext.sendBroadcastAsUser(closeAppWindow, UserHandle.ALL);
         final boolean keyguardOn = keyguardOn();
         int keyCode = event.getKeyCode();
         final int repeatCount = event.getRepeatCount();
