f0a54c18be2fdfee7e66e5dc8a0de9bb0f28abb2
Stevespear426 <stephen.k.spear@gmail.com>
2013-05-12 12:40:09 +0200

    FW: Ribbon Fix swipe ribbon and toggles for TabletUI
    Change-Id: I06408c0025001a7436342115c07055514bb3fd8c
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/NotificationPanel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/NotificationPanel.java
@@ -61,6 +61,7 @@ import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
 import com.android.systemui.statusbar.phone.SettingsPanelView;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 import com.android.systemui.statusbar.toggles.ToggleManager;
+import com.android.systemui.aokp.AokpSwipeRibbon;
 
 public class NotificationPanel extends RelativeLayout implements StatusBarPanel,
         View.OnClickListener {
@@ -88,6 +89,10 @@ public class NotificationPanel extends RelativeLayout implements StatusBarPanel,
 
     // settings
     ToggleManager mToggleManager;
+
+    private AokpSwipeRibbon mAokpSwipeRibbonLeft;
+    private AokpSwipeRibbon mAokpSwipeRibbonRight;
+    private AokpSwipeRibbon mAokpSwipeRibbonBottom;
     boolean mHasSettingsPanel, mHasFlipSettings;
     int mToggleStyle;
     SettingsPanelView mSettingsPanel;
@@ -124,6 +129,10 @@ public class NotificationPanel extends RelativeLayout implements StatusBarPanel,
             mToggleManager.setControllers(mBar.mBluetoothController,mBar.mNetworkController, mBar.mBatteryController,
                 mBar.mLocationController, null);
             mToggleManager.updateSettings();
+            mAokpSwipeRibbonLeft.setControllers(mBar.mBluetoothController,mBar.mNetworkController, mBar.mBatteryController,
+                mBar.mLocationController, null);
+            mAokpSwipeRibbonRight.setControllers(mBar.mBluetoothController,mBar.mNetworkController, mBar.mBatteryController,
+                mBar.mLocationController, null);
         }
     }
 
@@ -213,6 +222,9 @@ public class NotificationPanel extends RelativeLayout implements StatusBarPanel,
 
             // wherever you find it, Quick Settings needs a container to survive
             mToggleManager = new ToggleManager(mContext);
+            mAokpSwipeRibbonBottom = new AokpSwipeRibbon(mContext,null,"bottom");
+            mAokpSwipeRibbonLeft = new AokpSwipeRibbon(mContext,null,"left");
+            mAokpSwipeRibbonRight = new AokpSwipeRibbon(mContext,null,"right");
             mToggleStyle = Settings.System.getInt(mContext.getContentResolver(), 
                     Settings.System.TOGGLES_STYLE,ToggleManager.STYLE_TILE);
             if (mToggleStyle == ToggleManager.STYLE_SCROLLABLE) {
@@ -727,4 +739,3 @@ public class NotificationPanel extends RelativeLayout implements StatusBarPanel,
         }
     }
 }
-
