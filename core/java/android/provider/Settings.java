fb196089486db928e4e541814b25bbd9e2990472
burstlam <burstlam@gmail.com>
2013-05-31 23:57:13 +0800

    Revert "Framework: GlowPad Torch"
    This reverts commit 8d4deb679fe31a59131db341da61b2ebf2de47d8.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3432,12 +3432,6 @@ public final class Settings {
         };
 
         /**
-         *
-         * @hide
-         */
-        public static final String LOCKSCREEN_GLOW_TORCH = "lockscreen_glow_torch";
-
-        /**
          * Whether to use the custom quick unlock screen control
          * @hide
          */
