4d64025603cf080819a427d963eb5f0e4defc07b
martincz <martincz@mokeetek.com>
2013-06-11 21:55:04 +0800

    HALO: button align fix
    Change-Id: I521c104449f68e2cf7dc73b01fc90d9fc4ce2fa7
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/pieview/PieStatusPanel.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1698,7 +1698,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             }
         } else {
             mClearButton.setAlpha(clearable ? 1.0f : 0.0f);
-            mClearButton.setVisibility(clearable ? View.VISIBLE : View.INVISIBLE);
+            mClearButton.setVisibility(clearable ? View.VISIBLE : View.GONE);
         }
         mClearButton.setEnabled(clearable);
 
