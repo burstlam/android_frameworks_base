6351578adf1e32c2e5a553fc9cf41bcf6f434cec
Bajee11 <bajee11@gmail.com>
2013-06-20 02:12:40 -0400

    Revert "Core: Volume options panel 1/2"
    This reverts commit cae76b4251739ce52a87f6a4cbed3da0112cee2c.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Ia62f826aa5e8a9fb2dcc80913f3907ff79544629

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2929,13 +2929,6 @@ public final class Settings {
                 "navring_icon_4",
         };
 
-        /**
-         * Setting to enable volume options.
-         *
-         * @hide
-         */
-        public static final String ENABLE_VOLUME_OPTIONS = "enable_volume_options";
-
         /** 
          * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
          * Defaults to 1 - sounds enabled
