e860f8c15a058a7ee29b1ac6466d11451ce6ce2f
burstlam <burstlam@gmail.com>
2013-06-20 09:43:31 +0800

    Revert "FW : Allow user selection of Font Size"
    This reverts commit e1337211d285daa4a23a7d9464987f92f888d2a4.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
    
    Change-Id: Ic0f671668a2f3d7043baa2c7fd7f5c870a1a590d

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -48,7 +48,6 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import com.android.internal.util.aokp.StatusBarHelpers;
 import com.android.internal.R;
 
 /**
@@ -87,8 +86,6 @@ public class Clock extends TextView {
     protected int mClockStyle = STYLE_CLOCK_RIGHT;
 
     protected int mClockColor;
-    protected int mStockFontSize;
-    protected int mFontSize;
 
     public Clock(Context context) {
         this(context, null);
@@ -109,7 +106,6 @@ public class Clock extends TextView {
         if (!mAttached) {
             mAttached = true;
             mClockColor = getTextColors().getDefaultColor();
-            mStockFontSize = StatusBarHelpers.pixelsToSp(mContext,getTextSize());
             IntentFilter filter = new IntentFilter();
 
             filter.addAction(Intent.ACTION_TIME_TICK);
@@ -181,6 +177,9 @@ public class Clock extends TextView {
         } else {
             res = R.string.twelve_hour_time_format;
         }
+
+        final char MAGIC1 = '\uEF00';
+        final char MAGIC2 = '\uEF01';
 
         SimpleDateFormat sdf;
         String format = context.getString(res);
@@ -300,16 +299,12 @@ public class Clock extends TextView {
                 Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, CLOCK_DATE_DISPLAY_GONE);
         mClockDateStyle = Settings.System.getInt(resolver,
                 Settings.System.STATUSBAR_CLOCK_DATE_STYLE, CLOCK_DATE_STYLE_UPPERCASE);
-        mFontSize = Settings.System.getInt(resolver,
-			Settings.System.STATUSBAR_FONT_SIZE, mStockFontSize);
+
         newColor = Settings.System.getInt(resolver,
                 Settings.System.STATUSBAR_CLOCK_COLOR, mClockColor);
         if (newColor < 0 && newColor != mClockColor) {
             mClockColor = newColor;
             setTextColor(mClockColor);
-        }
-        if (mFontSize != getTextSize()){
-			setTextSize(mFontSize);
         }
         updateClockVisibility();
         updateClock();
