0d2ab47eefdafb8c19f6d61a9eaef13725b1a33c
0xD34D <clark@scheffsblend.com>
2013-06-24 19:55:11 -0700

    Add gesture based lockscreen security. [1/2] This adds the ability to us a hand drawn gesture as a means to unlock the device.
    In the case that the gesture is forgotton or not
    being recognized correctly, the fallback will be the
    same as when using patterns.
    
    PS2:
    Return SecurityMode.Account as backup for gesture login.
    
    PS3:
    Poke the wakelock when a gesture is detected so the screen
    will not timeout when an invalid gesture is entered.
    
    PS4:
    Clean things up a bit.
    
    Change-Id: I4ca8d7edff0a8906220cf4561d8b90896520e0cd
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/gesture/Gesture.aidl
    	core/java/android/gesture/GestureOverlayView.java
    	core/java/com/android/internal/widget/LockPatternUtils.java
    	core/java/com/android/internal/widget/LockSettingsService.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardSecurityModel.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardSecurityModel.java
@@ -38,6 +38,7 @@ public class KeyguardSecurityModel {
         None, // No security enabled
         Pattern, // Unlock by drawing a pattern.
         Password, // Unlock by entering an alphanumeric password
+        Gesture, // Unlock by drawing a gesture.
         PIN, // Strictly numeric password
         Biometric, // Unlock with a biometric key (e.g. finger print or face unlock)
         Account, // Unlock by entering an account's login and password.
@@ -141,6 +142,12 @@ public class KeyguardSecurityModel {
                         mode = SecurityMode.None;
                     }
                     break;
+                case DevicePolicyManager.PASSWORD_QUALITY_GESTURE_WEAK:
+                    if (mLockPatternUtils.isLockGestureEnabled()) {
+                        mode = mLockPatternUtils.isPermanentlyLocked() ?
+                            SecurityMode.Account : SecurityMode.Gesture;
+                    }
+                    break;
 
                 default:
                     throw new IllegalStateException("Unknown unlock mode:" + mode);
@@ -161,7 +168,8 @@ public class KeyguardSecurityModel {
         if (isBiometricUnlockEnabled() && !isBiometricUnlockSuppressed()
                 && (mode == SecurityMode.Password
                         || mode == SecurityMode.PIN
-                        || mode == SecurityMode.Pattern)) {
+                        || mode == SecurityMode.Pattern
+                        || mode == SecurityMode.Gesture )) {
             return SecurityMode.Biometric;
         }
         return mode; // no alternate, return what was given
@@ -178,6 +186,7 @@ public class KeyguardSecurityModel {
             case Biometric:
                 return getSecurityMode();
             case Pattern:
+            case Gesture:
                 return SecurityMode.Account;
         }
         return mode; // no backup, return current security mode
