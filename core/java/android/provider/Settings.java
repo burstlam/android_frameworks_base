187d40652fbde88129d83d29f3dbe37c51f328a4
Daniel MM <daxx4182@gmail.com>
2013-06-25 16:09:40 +0800

    HALO: Circle color
    Conflicts:
    	packages/SystemUI/res/drawable-hdpi/halo_bg.png
    
    Change-Id: I3566b0438ee3cd48cd3e9a4650b6e90b5fabfedb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3993,13 +3993,6 @@ public final class Settings {
 
         /**
          *
-         * HALO being minimal.
-         * @hide
-         */
-        public static final String HALO_STYLE = "halo_style";
-
-        /**
-         *
          * @hide
          */
         public static final String APP_WINDOW_HIDDEN_APPS = "app_window_hidden_apps";
@@ -4077,6 +4070,13 @@ public final class Settings {
             "ribbon_icon_location_left",
             "ribbon_icon_location_right",
         };
+
+        /**
+         *
+         * HALO circle bg color.
+         * @hide
+         */
+        public static final String HALO_CIRCLE_COLOR = "halo_circle_color";
 
         /**
          * HALO speech bubble color
