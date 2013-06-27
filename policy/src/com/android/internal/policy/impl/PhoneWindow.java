18ab173f39a4f86e82715b6ad2ee84575ba75bbb
burstlam <burstlam@gmail.com>
2013-06-27 14:15:37 +0800

    Revert "Move window manager calculation to first touch"
    This reverts commit 458c5fede2a320019e5bb502f2f247845101752b.

--- policy/src/com/android/internal/policy/impl/PhoneWindow.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindow.java
@@ -1876,8 +1876,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
 
         private boolean mEnableGestures;
 
-        private int mScreenWidth = 1;
-        private int mScreenHeight = -1;
+        private int mScreenWidth, mScreenHeight;
 
         public DecorView(Context context, int featureId) {
             super(context);
@@ -1951,18 +1950,14 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
             mShowGestures = Settings.System.getInt(resolver,
                     Settings.System.SHOW_GESTURES, 0) == 1;
 
-            invalidate();
-        }
-
-        private void updateScreenSize() {
             WindowManager wm = getWindowManager();
-            if (wm != null) {
-                Display display = wm.getDefaultDisplay();
-                Point size = new Point();
-                display.getRealSize(size);
-                mScreenWidth = size.x;
-                mScreenHeight = size.y;
-            }
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getRealSize(size);
+            mScreenWidth = size.x;
+            mScreenHeight = size.y;
+
+            invalidate();
         }
 
         @Override
@@ -2390,8 +2385,6 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
         @Override
         public boolean onInterceptTouchEvent(MotionEvent event) {
             int action = event.getAction();
-
-            if (mScreenWidth == -1 || mScreenHeight == -1) updateScreenSize();
 
             boolean mainPanel = getWidth() == mScreenWidth || getWidth() == mScreenHeight;
 
