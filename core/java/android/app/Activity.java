7ff1202dee763569de6d589c4ebd637fc0aebf97
PeterCxy <xqsx43cxy@126.com>
2013-08-28 11:41:39 +0800

    SwipeBackLayout: disable translucent on launcher activities

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -88,12 +88,14 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.AdapterView;
 import android.app.SwipeBackLayout;
 
+import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * An activity is a single, focused thing that the user can do.  Almost all
@@ -918,6 +920,23 @@ public class Activity extends ContextThemeWrapper
         mFragments.dispatchCreate();
         getApplication().dispatchActivityCreated(this, savedInstanceState);
         mCalled = true;
+        if (isCurrentHomeActivity()) {
+            // disable swipe back on launchers
+            setSwipeBackEnable(false);
+        }
+    }
+
+    /**
+     * get if this activity is home
+     *
+     */
+
+    private boolean isCurrentHomeActivity() {
+        ActivityInfo homeInfo = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME)
+                .resolveActivityInfo(this.getPackageManager(), 0);
+        return homeInfo != null
+                && homeInfo.packageName.equals(this.getPackageName())
+                && homeInfo.name.equals(this.getClass().getName());
     }
 
     /**
