0aae1beb25ee675c0979d67ff997c0ef51f10e40
burstlam <burstlam@gmail.com>
2013-09-22 03:50:00 +0800

    Port Active Display from ChameleonOs (1/2)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -633,6 +633,8 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
         if (mRecreating) {
             removeSidebarView();
+        } else {
+            addActiveDisplayView();
         }
         addSidebarView();
 
@@ -2781,7 +2783,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         setAreThereNotifications();
     }
 
-    private boolean areLightsOn() {
+    public boolean areLightsOn() {
         return 0 == (mSystemUiVisibility & View.SYSTEM_UI_FLAG_LOW_PROFILE);
     }
 
@@ -2799,6 +2801,10 @@ public class PhoneStatusBar extends BaseStatusBar {
             mWindowManagerService.statusBarVisibilityChanged(mSystemUiVisibility);
         } catch (RemoteException ex) {
         }
+    }
+
+    public void setNavigationBarLightsOn(boolean on, boolean force) {
+        mNavigationBarView.setLowProfile(!on, true, force);
     }
 
     @Override
