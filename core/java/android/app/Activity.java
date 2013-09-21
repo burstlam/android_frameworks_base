196ec65f0d7b637879b59d6794af26868b392907
PeterCxy <xqsx43cxy@126.com>
2013-09-21 10:43:34 +0800

    SwipeBackLayout: Blacklist (1/2) As we know, many apps do not support Swipe Back Layout now. If enable Swipe Back Layout on these apps, they may not work. So now I imported a Blacklist feature to let user choose whether to disable swipe back layout on some apps or not This patch contains a set of functions to read/write blacklist for Swipe Back Layout. Another necessary patch is in packages/apps/Settings.

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -16,6 +16,10 @@
 
 package android.app;
 
+import static org.xmlpull.v1.XmlPullParser.END_DOCUMENT;
+import static org.xmlpull.v1.XmlPullParser.END_TAG;
+import static org.xmlpull.v1.XmlPullParser.START_TAG;
+
 import com.android.internal.app.ActionBarImpl;
 import com.android.internal.policy.PolicyManager;
 
@@ -64,6 +68,8 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.util.Slog;
 import android.util.SparseArray;
+import android.util.AtomicFile;
+import android.util.Xml;
 import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
@@ -90,12 +96,26 @@ import android.app.SwipeBackLayout;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
+import com.android.internal.util.FastXmlSerializer;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+
+import libcore.io.IoUtils;
 
 /**
  * An activity is a single, focused thing that the user can do.  Almost all
@@ -674,6 +694,16 @@ public class Activity extends ContextThemeWrapper
     private static final String SAVED_DIALOG_KEY_PREFIX = "android:dialog_";
     private static final String SAVED_DIALOG_ARGS_KEY_PREFIX = "android:dialog_args_";
 
+    private static final String TAG_BODY = "swipeback-policy";
+    private static final String ATTR_VERSION = "version";
+    private static final String ATTR_NAME = "name";
+    private static final String TAG_PACKAGE = "package";
+    private static final String TAG_BLOCKED_APPS = "blocked_apps";
+    private static final int DB_VERSION = 1;
+
+    private static AtomicFile mSwipeBackPolicyFile;
+    private static HashSet<String> mSwipeBackBlacklist = new HashSet<String>();
+
     private static class ManagedDialog {
         Dialog mDialog;
         Bundle mArgs;
@@ -920,8 +950,11 @@ public class Activity extends ContextThemeWrapper
         mFragments.dispatchCreate();
         getApplication().dispatchActivityCreated(this, savedInstanceState);
         mCalled = true;
-        if (isCurrentHomeActivity()) {
-            // disable swipe back on launchers
+        if (mSwipeBackPolicyFile == null) {
+            loadSwipeBackBlockDb();
+        }
+        if (isCurrentHomeActivity() || !isAllowedForSwipeBack(this.getPackageName())) {
+            // disable swipe back on launchers or blocked
             setSwipeBackEnable(false);
         }
     }
@@ -939,6 +972,125 @@ public class Activity extends ContextThemeWrapper
                 && homeInfo.name.equals(this.getClass().getName());
     }
 
+    /**
+     * read policy db
+     * pulled from NotificationManagerService.java
+     *
+     */
+    private static int readPolicy(AtomicFile file, String lookUpTag, HashSet<String> db) {
+        return readPolicy(file, lookUpTag, db, null, 0);
+    }
+
+    private static int readPolicy(AtomicFile file, String lookUpTag, HashSet<String> db, String resultTag, int defaultResult) {
+        int result = defaultResult;
+        FileInputStream infile = null;
+        try {
+            infile = file.openRead();
+            final XmlPullParser parser = Xml.newPullParser();
+            parser.setInput(infile, null);
+
+            int type;
+            String tag;
+            int version = DB_VERSION;
+            while ((type = parser.next()) != END_DOCUMENT) {
+                tag = parser.getName();
+                if (type == START_TAG) {
+                    if (TAG_BODY.equals(tag)) {
+                        version = Integer.parseInt(parser.getAttributeValue(null, ATTR_VERSION));
+                        if (resultTag != null) {
+                            String attribValue = parser.getAttributeValue(null, resultTag);
+                            result = Integer.parseInt((attribValue != null ? attribValue : "0"));
+                        }
+                    } else if (lookUpTag.equals(tag)) {
+                        while ((type = parser.next()) != END_DOCUMENT) {
+                            tag = parser.getName();
+                            if (TAG_PACKAGE.equals(tag)) {
+                                db.add(parser.getAttributeValue(null, ATTR_NAME));
+                            } else if (lookUpTag.equals(tag) && type == END_TAG) {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // Unable to read
+        } finally {
+            IoUtils.closeQuietly(infile);
+        }
+        return result;
+    }
+
+    /**
+     * Read Db to store apps which are blocked
+     *
+     */
+    protected static synchronized void loadSwipeBackBlockDb() {
+        mSwipeBackPolicyFile = new AtomicFile(new File("/data/system", "swipeback_policy.xml"));
+        mSwipeBackBlacklist.clear();
+        readPolicy(mSwipeBackPolicyFile, TAG_BLOCKED_APPS, mSwipeBackBlacklist);
+     }
+     
+    /**
+     * Write Db
+     *
+     */
+    private static synchronized void writeSwipeBackBlockDb() {
+        FileOutputStream outfile = null;
+        try {
+            outfile = mSwipeBackPolicyFile.startWrite();
+
+            XmlSerializer out = new FastXmlSerializer();
+            out.setOutput(outfile, "utf-8");
+
+            out.startDocument(null, true);
+
+            out.startTag(null, TAG_BODY); {
+                out.attribute(null, ATTR_VERSION, String.valueOf(DB_VERSION));
+
+                    out.startTag(null, TAG_BLOCKED_APPS); {
+                        for (String blockedPkg : mSwipeBackBlacklist) {
+                            out.startTag(null, TAG_PACKAGE); {
+                                out.attribute(null, ATTR_NAME, blockedPkg);
+                            } out.endTag(null, TAG_PACKAGE);
+                        }
+                    } out.endTag(null, TAG_BLOCKED_APPS);
+                   
+
+            } out.endTag(null, TAG_BODY);
+
+            out.endDocument();
+
+            mSwipeBackPolicyFile.finishWrite(outfile);
+        } catch (IOException e) {
+            if (outfile != null) {
+                mSwipeBackPolicyFile.failWrite(outfile);
+            }
+        }
+    }
+
+    /**
+     * If this app is allowed for SwipeBackLayout return true
+     *
+     */
+    public static boolean isAllowedForSwipeBack(String pkg) {
+        return !mSwipeBackBlacklist.contains(pkg);
+    }
+    
+    /**
+     * Set if an app is blocked or allowed for SwipeBackLayout
+     *
+     */
+    public static void setSwipeBackBlacklistStatus(String pkg, boolean status) {
+        if (status) {
+            mSwipeBackBlacklist.add(pkg);
+        } else {
+            mSwipeBackBlacklist.remove(pkg);
+        }
+        writeSwipeBackBlockDb();
+        loadSwipeBackBlockDb();
+    }
+    
     /**
      * The hook for {@link ActivityThread} to restore the state of this activity.
      *
@@ -1051,7 +1203,7 @@ public class Activity extends ContextThemeWrapper
             onTitleChanged(getTitle(), getTitleColor());
         }
         mCalled = true;
-        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1);
+        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1) && isAllowedForSwipeBack(this.getPackageName());
         if (mSwipeBackEnabled == true && mSwipeBackLayout == null) {
             getWindow().setBackgroundDrawable(new ColorDrawable(0));
             getWindow().getDecorView().setBackgroundDrawable(null);
@@ -1927,7 +2079,7 @@ public class Activity extends ContextThemeWrapper
         if (v != null)
             return v;
 
-        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1);
+        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1) && isAllowedForSwipeBack(this.getPackageName());
         if (mSwipeBackEnabled == true && mSwipeBackLayout == null) {
             getWindow().setBackgroundDrawable(new ColorDrawable(0));
             getWindow().getDecorView().setBackgroundDrawable(null);
@@ -1966,7 +2118,7 @@ public class Activity extends ContextThemeWrapper
      * @return The layout used for swipe back gestures
      */
     public SwipeBackLayout getSwipeBackLayout() {
-        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1);
+        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1) && isAllowedForSwipeBack(this.getPackageName());
         if (mSwipeBackEnabled == true && mSwipeBackLayout == null) {
             getWindow().setBackgroundDrawable(new ColorDrawable(0));
             getWindow().getDecorView().setBackgroundDrawable(null);
@@ -2004,7 +2156,7 @@ public class Activity extends ContextThemeWrapper
      *
      */
     public void setSwipeBackEnable(boolean enable) {
-        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1);
+        mSwipeBackEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.SWIPE_BACK_GESTURE_ENABLED, 0) == 1) && isAllowedForSwipeBack(this.getPackageName());
         if (mSwipeBackEnabled == true && mSwipeBackLayout == null) {
             // getWindow().setBackgroundDrawable(new ColorDrawable(0));
             // getWindow().getDecorView().setBackgroundDrawable(null);
