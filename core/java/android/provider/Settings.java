17601a009c3778f67d381e30ccd59912f7766f1c
burstlam <burstlam@gmail.com>
2013-09-24 03:42:36 +0800

    Recents Panel : option to add Google Assist Button (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4244,6 +4244,13 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER = "status_bar_carrier";
 
         /**
+         * Google Assistant in Recents
+         *
+         * @hide
+         */
+        public static final String RECENTS_TARGET_ASSIST = "recents_target_assist";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
