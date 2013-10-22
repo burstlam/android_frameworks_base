a1fb00b12eee1638c285c54fff1b5b23741d6039
Dan Pasanen <dan.pasanen@gmail.com>
2013-10-22 09:39:23 -0500

    QuietHours: clean up and consolidate code
    Change-Id: Ib745ba91f800489518f1c7b5d2512ba946039696
    
    Conflicts:
    	services/java/com/android/server/BatteryService.java
    	services/java/com/android/server/NotificationManagerService.java
    	services/java/com/android/server/VibratorService.java

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -43,8 +43,9 @@ import android.view.Surface;
 import android.view.VolumePanel;
 import android.view.WindowManager;
 
-import java.util.Calendar;
 import java.util.HashMap;
+
+import com.android.internal.util.cm.QuietHoursUtils;
 
 /**
  * AudioManager provides access to volume and ringer mode control.
@@ -1767,7 +1768,7 @@ public class AudioManager {
             return;
         }
 
-        if (inQuietHours()) {
+        if (QuietHoursUtils.inQuietHours(mContext, Settings.System.QUIET_HOURS_SYSTEM)) {
             return;
         }
 
@@ -1812,29 +1813,6 @@ public class AudioManager {
         } catch (RemoteException e) {
             Log.e(TAG, "Dead object in playSoundEffect"+e);
         }
-    }
-
-    public boolean inQuietHours() {
-        boolean quietHoursEnabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_ENABLED, 0) != 0;
-        int quietHoursStart = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_START, 0);
-        int quietHoursEnd = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_END, 0);
-        boolean quietHoursSystem = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.QUIET_HOURS_SYSTEM, 0) != 0;
-        if (quietHoursEnabled && quietHoursSystem && (quietHoursStart != quietHoursEnd)) {
-            // Get the date in "quiet hours" format.
-            Calendar calendar = Calendar.getInstance();
-            int minutes = calendar.get(Calendar.HOUR_OF_DAY) * 60 + calendar.get(Calendar.MINUTE);
-            if (quietHoursEnd < quietHoursStart) {
-                // Starts at night, ends in the morning.
-                return (minutes > quietHoursStart) || (minutes < quietHoursEnd);
-            } else {
-                return (minutes > quietHoursStart) && (minutes < quietHoursEnd);
-            }
-        }
-        return false;
     }
 
 
