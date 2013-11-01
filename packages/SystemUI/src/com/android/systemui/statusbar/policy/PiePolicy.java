09f6b8defc771877bdd34faf8c9c8e6624374903
burstlam <burstlam@gmail.com>
2013-11-02 00:57:09 +0800

    PA Pie : Add SPN and Custom Carrier Label

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PiePolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PiePolicy.java
@@ -21,16 +21,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.text.format.DateFormat;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 
 import com.android.systemui.R;
+import com.android.systemui.statusbar.util.SpnOverride;
 
 import java.text.SimpleDateFormat;
+import android.text.format.DateFormat;
+import android.text.TextUtils;
 import java.util.Date;
 
 public class PiePolicy {
@@ -41,6 +44,7 @@ public class PiePolicy {
     private static Context mContext;
     private static int mBatteryLevel = 0;
     private static boolean mTelephony;
+    private static boolean isCN;
 
     private OnClockChangedListener mClockChangedListener;
 
@@ -55,6 +59,7 @@ public class PiePolicy {
         @Override
         public void onReceive(Context context, Intent intent) {
             mClockChangedListener.onChange(getSimpleTime());
+            isCN = mContext.getResources().getConfiguration().locale.getCountry().equals("CN") || mContext.getResources().getConfiguration().locale.getCountry().equals("TW");
         }
     };
 
@@ -69,12 +74,14 @@ public class PiePolicy {
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_TIME_TICK);
         filter.addAction(Intent.ACTION_TIME_CHANGED);
+        filter.addAction(Intent.ACTION_LOCALE_CHANGED);
         mContext.registerReceiver(mClockReceiver, filter);
         LOW_BATTERY_LEVEL = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_lowBatteryWarningLevel);
         CRITICAL_BATTERY_LEVEL = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
         mTelephony = mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+        isCN = mContext.getResources().getConfiguration().locale.getCountry().equals("CN") || mContext.getResources().getConfiguration().locale.getCountry().equals("TW");
     }
 
     public void setOnClockChangedListener(OnClockChangedListener l){
@@ -100,12 +107,25 @@ public class PiePolicy {
     }
 
     public static String getNetworkProvider() {
-        String operatorName = mContext.getString(R.string.quick_settings_wifi_no_network);
-        TelephonyManager telephonyManager = (TelephonyManager) mContext
-                .getSystemService(Context.TELEPHONY_SERVICE);
-        operatorName = telephonyManager.getNetworkOperatorName();
-        if(operatorName == null) {
-            operatorName = telephonyManager.getSimOperatorName();
+        String operatorName = Settings.System.getString(mContext.getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL);
+        if(TextUtils.isEmpty(operatorName)) {
+        operatorName = mContext.getString(R.string.quick_settings_wifi_no_network);
+        TelephonyManager telephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+            if(isCN) {
+                String operator = telephonyManager.getNetworkOperator();
+                if(TextUtils.isEmpty(operator))
+                    operator = telephonyManager.getSimOperator();
+                SpnOverride mSpnOverride = new SpnOverride();
+                operatorName = mSpnOverride.getSpn(operator);
+                if(operatorName == null) {
+                    operatorName = telephonyManager.getSimOperatorName();
+                }
+            } else {
+                operatorName = telephonyManager.getNetworkOperatorName();
+                if(operatorName == null) {
+                    operatorName = telephonyManager.getSimOperatorName();
+                }
+            }
         }
         return operatorName.toUpperCase();
     }
