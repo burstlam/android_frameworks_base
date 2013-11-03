817d9d338702870cbb2816f3f9e258902d1b7038
burstlam <burstlam@gmail.com>
2013-11-04 06:40:07 +0800

    Fix StatusIcon Opacity

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarIconView.java
@@ -374,8 +374,8 @@ public class StatusBarIconView extends AnimatedImageView {
 
         @Override
         public void onChange(boolean selfChange) {
-            setAlpha(Settings.System.getInt(mContext.getContentResolver(),
-            Settings.System.STATUS_BAR_NOTIF_ICON_OPACITY, 140));
+            //setAlpha(Settings.System.getInt(mContext.getContentResolver(),
+            //Settings.System.STATUS_BAR_NOTIF_ICON_OPACITY, 140));
             boolean showIconCount = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.STATUSBAR_NOTIF_COUNT, 0) == 1;
             for (StatusBarIconView sbiv : mIconViews) {
