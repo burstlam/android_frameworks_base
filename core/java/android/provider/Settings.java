7d979772911f7dba5fc118779ddefdec566d656f
Sergiu Sandor <micku7zu@gmail.com>
2013-12-07 18:09:05 +0200

    [1/2] Base: Fix pimp up ActiveDisplay
    This is a fix for the new features added with change: https://gerrit.omnirom.org/#/c/2819/
    
    1. Fix the bug when Pocket mode is set to Notifications only and you
    unlock your device with power key and if you put your hands over
    proximity sensor the device will turn off (very annoying). The problem
    was that mWakedByPocketMode it was put in wrong place.
    
    2. Added an option in active display settings for "turn off display if
    device gets pocketed again" (it's annoying for me, because i cover
    the proximity very often).
    
    3. Added an option in active display settings for proximity threshold.
    I use this active display feature as an replacement for "sweep2wake",
    and when my phone it's on the table, i just want to pass my hand over
    it to show me the clock or notification (this was ok before the pimp up
    commit), and now, you can choose how many seconds for proximity
    threshold.
    
    Practicaly, this commit fix one bug, and add settings for the
    features added in 'pimp up' commit.
    
    [2/2] part: https://gerrit.omnirom.org/#/c/3381/
    
    Change-Id: I9e49ddb89002699714dcc3b3d2b1df87dc4bab4b
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2637,10 +2637,22 @@ public final class Settings {
         public static final String ACTIVE_DISPLAY_SUNLIGHT_MODE = "active_display_sunlight_mode";
 
         /**
+         * Whether to turn off the device when gets pocketed again and was waked up by active display
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_TURNOFF_MODE = "active_display_turnoff_mode";
+
+        /**
+         * Threshold of the proximity sensor to turn on the device.
+         * @hide
+         */
+        public static final String ACTIVE_DISPLAY_THRESHOLD = "active_display_threshold";
+
+        /**
          * Display timeout for Active Display
          * @hide
          */
-        public static final String ACTIVE_DISPLAY_DISPLAYTIMEOUT = "active_display_displaytimeout";
+        public static final String ACTIVE_DISPLAY_TIMEOUT = "active_display_timeout";
 
         /**
          * A list of packages to exclude from being displayed in active display.
